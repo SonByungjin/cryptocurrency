1abdf214cba33d306bcd838d89b35c027c888187
sonbyungjin <sgyos000@gmail.com>
2020-10-06 20:04:38 +0900

    리팩토링

--- src/Pages/Profit/Profit.js
+++ src/Pages/Profit/Profit.js
@@ -1,30 +1,81 @@
-import React, { useEffect, useState } from 'react';
-import styled, { css } from 'styled-components';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Nav from '../../Components/Nav/Nav';
+import Footer from '../../Components/Footer/Footer';
+
+const barMenu = [
+  '코인명',
+  '보유수량',
+  '매수 평균가',
+  '매수 금액',
+  '평가 금액',
+  '수익률',
+];
 
 const Profit = () => {
-  const [BarMenu, setBarMenu] = useState([
-    '코인명',
-    '보유수량',
-    '매수 평균가',
-    '매수 금액',
-    '평가 금액',
-    '수익률',
-  ]);
+  const [tradingData, setTradingData] = useState({
+    // redux로 데이터를 받아올 예정 레이아웃 작업으로 인해 미리 설정해놓았습니다.
+    coin_list: [
+      {
+        icon:
+          'https://cdn.pixabay.com/photo/2016/11/05/20/08/sunflower-1801284_960_720.png',
+        coin_name: '썬쓰',
+        coin_code: 'SUNS',
+        quantity: 251.0,
+        average_buying_price: {
+          price__avg: 1000.0,
+        },
+        buying_price: 1000.0,
+        current_price: 1000.0,
+        profit_rate: 0,
+      },
+      {
+        icon:
+          'https://cdn.pixabay.com/photo/2012/04/24/12/33/circle-39783_960_720.png',
+        coin_name: '피블쓰',
+        coin_code: 'PIBS',
+        quantity: 10.0,
+        average_buying_price: {
+          price__avg: 1000.0,
+        },
+        buying_price: 1000.0,
+        current_price: 1000.0,
+        profit_rate: 0,
+      },
+    ],
+    total_asset: 2285594804.0,
+    won_balance: 2285333804.0,
+    coin_balance: 261000.0,
+    total_buy_price: 261000.0,
+    profit: -47.4,
+    profit_rate: 0,
+  });
+  const {
+    coin_list,
+    total_asset,
+    won_balance,
+    coin_balance,
+    total_buy_price,
+    profit,
+    profit_rate,
+  } = tradingData;
 
-  const [TradingData, setTradingData] = useState([
-    {
-      coinName: 'MATIC',
-      amount: 45.1,
-      average: 21.9,
-      tradingPrice: 654,
-      evaluation: 516,
-      profitPercentage: 14.5,
-    },
-  ]);
+  // useEffect로 실행할 예정입니다
+  const getInitialData = async () => {
+    const res = await fetch('api', {
+      headers: {
+        Authorization: localStorage.getItem('token'),
+      },
+    });
+    const firstTradingData = await res.json();
+
+    //여기에다가 dispatch 해야할 에정입니다
+    setTradingData(firstTradingData);
+  };
 
   return (
     <>
-      <div>nav 예정</div>
+      <Nav />
       <ProfitWrap>
         <MenuVertical>
           <ul>
@@ -38,30 +89,30 @@ const Profit = () => {
           <TotalAssetContainer>
             <TotalAsset>
               <span>총 보유자산</span>
-              <span>{(5000).toLocaleString('en')}원</span>
+              <span>{total_asset.toLocaleString('en')}원</span>
             </TotalAsset>
             <OwnAsset>
               <ItemAndPrice>
                 <span>보유 원화</span>
-                <span>{(4000).toLocaleString('en')}원</span>
+                <span>{won_balance.toLocaleString('en')}원</span>
               </ItemAndPrice>
               <ItemAndPrice>
                 <span>보유 암호화폐</span>
-                <span>{(1000).toLocaleString('en')}원</span>
+                <span>{coin_balance.toLocaleString('en')}원</span>
               </ItemAndPrice>
             </OwnAsset>
             <TradingAsset>
               <ItemAndPrice>
                 <span>총매수금액</span>
-                <span>{(1000).toLocaleString('en')}원</span>
+                <span>{total_buy_price.toLocaleString('en')}원</span>
               </ItemAndPrice>
               <ItemAndPrice>
                 <span>평가 손익</span>
-                <span>-183원</span>
+                <span>{profit}원</span>
               </ItemAndPrice>
               <ItemAndPrice>
                 <span>수익률</span>
-                <span>-15.33%</span>
+                <span>{profit_rate}%</span>
               </ItemAndPrice>
             </TradingAsset>
           </TotalAssetContainer>
@@ -72,29 +123,40 @@ const Profit = () => {
             </p>
             <AssetByOrder>
               <AssetByOrderBar>
-                {BarMenu.map((menu, menuIdx) => (
+                {barMenu.map((menu, menuIdx) => (
                   <div key={menuIdx}>
                     <span>{menu}</span>
                   </div>
                 ))}
               </AssetByOrderBar>
               <AssetByOrderList>
-                {TradingData.map((trade, tradeIdx) => (
-                  <ul key={tradeIdx}>
-                    <li>{trade.coinName}</li>
-                    <li>{trade.amount}</li>
-                    <li>{trade.average}</li>
-                    <li>{trade.tradingPrice}</li>
-                    <li>{trade.evaluation}</li>
-                    <li>{trade.profitPercentage}</li>
-                  </ul>
-                ))}
+                {coin_list.map((trade, tradeIdx) => {
+                  return (
+                    <ul key={tradeIdx}>
+                      <li>
+                        <div>
+                          <img alt="coinImg" src={trade.icon} />
+                          {trade.coin_code}
+                        </div>
+                        <span>{trade.coin_name}</span>
+                      </li>
+                      <li>{trade.quantity}</li>
+                      <li>
+                        {trade.average_buying_price.price__avg.toLocaleString()}
+                        원
+                      </li>
+                      <li>{trade.buying_price.toLocaleString()}원</li>
+                      <li>{trade.current_price.toLocaleString()}원</li>
+                      <li>{trade.profit_rate}%</li>
+                    </ul>
+                  );
+                })}
               </AssetByOrderList>
             </AssetByOrder>
           </AssetByOrderContainer>
         </ProfitStatusContainer>
       </ProfitWrap>
-      <div>footer 예정</div>
+      <Footer />
     </>
   );
 };
@@ -102,8 +164,8 @@ const Profit = () => {
 export default Profit;
 
 const ProfitWrap = styled.div`
+  margin-top: 50px;
   padding: 80px 0;
-  width: 100%;
   height: 800px;
   ${({ theme }) => theme.flex('center', 'center')};
 `;
@@ -133,8 +195,7 @@ const ProfitStatusContainer = styled.div`
 const TotalAssetContainer = styled.div`
   width: 100%;
   height: 130px;
-  margin-top: 50px;
-  margin-bottom: 50px;
+  margin: 50px 0;
   border-top: 1px solid #c9ccd2;
   border-bottom: 1px solid #c9ccd2;
   ${({ theme }) => theme.flex('center', 'center')};
@@ -174,10 +235,8 @@ const TradingAsset = styled.div`
   width: 33%;
   height: 100%;
   ${({ theme }) => theme.flex('center', 'center', 'column')}
-  div {
-    span {
-      color: #79818f;
-    }
+  span {
+    color: #79818f;
   }
 `;
 
@@ -219,12 +278,23 @@ const AssetByOrderList = styled.div`
     border-bottom: 1px solid #c9ccd2;
     ${({ theme }) => theme.flex('center', 'center')};
     li {
+      width: 15%;
       padding-left: 10px;
       font-size: 14px;
       :first-child {
         width: 25%;
+        div {
+          ${({ theme }) => theme.flex(null, 'center')};
+          img {
+            margin: 5px;
+            width: 20px;
+          }
+        }
+        span {
+          margin-left: 5px;
+          ${({ theme }) => theme.text('5px', null, '#79818f')}
+        }
       }
-      width: 15%;
     }
   }
 `;
